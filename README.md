888c7a4024a32585e7dafb658060a4f291c4ffbf
Miguel Angel López Arcos <miguel.arcos@encora.com>
2024-04-24 10:32:42 -0600

    Finish implementation of Fees module

--- README.md
+++ README.md
@@ -18,3 +18,5 @@ There is a controller to create ***JWT*** tokens for this user (There is a examp
 All the operations that involve card management need a ***JWT*** to work, so, first you need to create the ***JWT*** and then pass this ***JWT*** to Card Management requests in the **Authorization Header** (Theorically every time you run a ***sign-in*** or ***sign-up*** in postman the token is stored automatically in the collection variables).
 
 If you run the project using ***docker compose*** then the URL is this ***http://localhost:5001***, if you run it on your local machine the ***launchSettings.json*** file is configured to use the following URLs ***https://localhost:8001;http://localhost:8000*** (One for ***https*** and one for ***http***)
+
+Note: Every time you do a SignUp the token generated overwrites your token in the collection variables leading to possible errors like ***"card-does-not-belong-to-user"*** if you use the same CardId with this new token
--- src/RapidPay.Domain/Services/PaymentFeesModule.cs
+++ src/RapidPay.Domain/Services/PaymentFeesModule.cs
@@ -2,9 +2,27 @@ namespace RapidPay.Domain.Services;
 
 public class PaymentFeesModule : IPaymentFeesModule
 {
+    private decimal currentFee;
+    private DateTime nextFeeDate;
+    private const int numberOfSecondsBetweenEachFeeChange = 3600;
+
+    public PaymentFeesModule() {
+        currentFee = GenerateRandomeFee();
+        nextFeeDate = DateTime.Now.AddSeconds(numberOfSecondsBetweenEachFeeChange);
+    }
+
     public decimal GetFee()
     {
-       var rawFee = new Random().NextDouble() * 2;
-       return Math.Round(Convert.ToDecimal(rawFee), 4);
+        var now = DateTime.Now;
+        if (now > nextFeeDate) {
+            currentFee *= GenerateRandomeFee();
+            nextFeeDate = now.AddSeconds(numberOfSecondsBetweenEachFeeChange);
+        }
+        return currentFee;
+    }
+
+    private decimal GenerateRandomeFee() {
+        var rawFee = new Random().NextDouble() * 2; // Generate number from 0 to 2
+        return Math.Round(Convert.ToDecimal(rawFee), 4);
     }
 }
